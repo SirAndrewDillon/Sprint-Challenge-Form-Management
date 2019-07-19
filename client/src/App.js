c0c356ed43a9ac597cb82083d70347d4f650a801
andrew dillon <majik895@gmail.com>
2019-07-19 13:12:38 -0400

    accepting tokens

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 // import { Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/RegistrationForm';
+import Menu from './components/Menu'
 import ShowData from './components/ShowData';
+import JqueryMenu from './components/JqueryMenu'
 
 function App() {
   return (
     <div className='App'>
+      <Menu />
       <Login />
       <ShowData />
     </div>
--- client/src/components/ShowData.jsx
+++ client/src/components/ShowData.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import axiosWithAuth from "./axiosWithAuth";
 
 export default function ShowData() {
-  return <div />;
+  const [data, updateData] = useState([]);
+  console.log("Data is : ", data);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/api/restricted/data")
+      .then(res => {
+        updateData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      {data.map(recipe => {
+        return (
+          <div key={recipe.name} className="recipe-card-wrapper">
+            <h2>{recipe.name}</h2>
+            <h3>{recipe.course}</h3>
+            <h3>{recipe.technique}</h3>
+            {recipe.ingredients.map(ingredient => {
+              return <p>{ingredient}</p>;
+            })}
+          </div>
+        );
+      })}
+    </>
+  );
 }
