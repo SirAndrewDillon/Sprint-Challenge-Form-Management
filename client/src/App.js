27cc52c8067a2e5292ec738ff885408f6bbceb40
andrew dillon <majik895@gmail.com>
2019-07-19 15:26:03 -0400

    working crud

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
-// import { Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Link } from 'react-router-dom'; 
+import PrivateRoute from './utilities/PrivateRoute';
+import Register from './components/Register';
+import FoodList from './components/FoodList';
+import useLocalStorage from '../src/hooks/useLocalStorage';
 import './App.css';
-import Login from './components/RegistrationForm';
-import Menu from './components/Menu'
-import ShowData from './components/ShowData';
-import JqueryMenu from './components/JqueryMenu'
 
 function App() {
+  const [token, setToken] = useLocalStorage('userToken')
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    if (token) {
+      setIsLoggedIn(true);
+      console.log('testing')
+    }
+  }, [token])
+
+  const logout = (event) => {
+    event.preventDefault();
+    localStorage.removeItem('userToken')
+    setIsLoggedIn(false);
+  }
+
   return (
-    <div className='App'>
-      <Menu />
-      <Login />
-      <ShowData />
+    <div className="app">
+    <header className='app-header'>
+      {!isLoggedIn ? <Link to='/' >Register</Link> : <Link to='/' onClick={logout}>Logout</Link> }
+      <Link to='/content'>Content</Link>
+    </header>
+    <div className='app-content'>
+      <Route exact path='/' render={(props) => <Register {...props} setToken={setToken} setIsLoggedIn={setIsLoggedIn} />} />
+      <PrivateRoute exact path='/content' component={FoodList}/>
     </div>
+  </div>
   );
 }
 
